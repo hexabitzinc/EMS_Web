6b4ee243e3334f4867f92be78654ecd35ce4bba0
Hani Mounla <hani.mounla@outlook.com>
2019-07-07 19:52:38 +0300

    Finished zoning database section

--- EMS/EMS.Web/Migrations/DefaultDB/DefaultDB_20190101_130000_EMS.cs
+++ EMS/EMS.Web/Migrations/DefaultDB/DefaultDB_20190101_130000_EMS.cs
@@ -9,24 +9,46 @@ namespace EMS.Migrations.DefaultDB
         {
             // EMS Migrations
 
-            #region zoning
+            #region Zoning
 
             Create.Table("City")
-                   .WithColumn("CityID").AsInt32().Identity().PrimaryKey().NotNullable()
-                   .WithColumn("Name").AsString(200).NotNullable();
+                   .WithColumn("CityID").AsInt32().Identity().PrimaryKey()
+                   .WithColumn("Name").AsString(200).NotNullable()
+                   .WithColumn("Deleted").AsBoolean().Nullable();
 
             Create.Table("Building")
-                    .WithColumn("BuildingID").AsInt32().Identity().PrimaryKey().NotNullable()
-                    .WithColumn("Name").AsString(200).NotNullable();
+                    .WithColumn("BuildingID").AsInt32().Identity().PrimaryKey()
+                    .WithColumn("ZipCode").AsString(200).Nullable()
+                    .WithColumn("Address").AsString(200).Nullable()
+                    .WithColumn("StreetAddress").AsString(200).Nullable()
+                    .WithColumn("Description").AsString(200).Nullable()
+                    .WithColumn("CityID").AsInt32().Nullable().ForeignKey("FK_Building_City", "dbo", "City", "CityID")
+                    .WithColumn("Deleted").AsBoolean().Nullable();
 
             Create.Table("Floor")
-                    .WithColumn("FloorID").AsInt32().Identity().PrimaryKey().NotNullable()
-                    .WithColumn("BuildingID").AsString(200).NotNullable();
+                    .WithColumn("FloorID").AsInt32().Identity().PrimaryKey()
+                    .WithColumn("FloorNumber").AsInt32().Nullable()
+                    .WithColumn("Name").AsString(200).Nullable()
+                    .WithColumn("Describtion").AsString(200).Nullable()
+                    .WithColumn("BuildingID").AsInt32().Nullable().ForeignKey("FK_Floor_Building", "dbo", "Building", "BuildingID")
+                    .WithColumn("Deleted").AsBoolean().Nullable();
 
-
-            Alter.Table("Building")
-                .AddColumn("CityID").AsInt32().Nullable()
-                    .ForeignKey("FK_Building_city", "dbo", "City", "CityID");
+            Create.Table("Section")
+                    .WithColumn("SectionID").AsInt32().Identity().PrimaryKey()
+                    .WithColumn("Name").AsString(200).Nullable()
+                    .WithColumn("Describtion").AsString(200).Nullable()
+                    .WithColumn("FloorID").AsInt32().Nullable().ForeignKey("FK_Section_Floor", "dbo", "Floor", "FloorID")
+                    .WithColumn("Deleted").AsBoolean().Nullable();
+            
+            Create.Table("Apartment")
+                    .WithColumn("ApartmentID").AsInt32().Identity().PrimaryKey()
+                    .WithColumn("Name").AsString(200).Nullable()
+                    .WithColumn("Type").AsString(200).Nullable()
+                    .WithColumn("Address").AsString(200).Nullable()
+                    .WithColumn("RoomNumber").AsString(200).Nullable()
+                    .WithColumn("Describtion").AsString(200).Nullable()
+                    .WithColumn("SectionID").AsInt32().Nullable().ForeignKey("FK_Apartment_Section", "dbo", "Section", "SectionID")
+                    .WithColumn("Deleted").AsBoolean().Nullable();
 
             #endregion
 
