73ed43c184ab035751f40e5a818f774619fa7c42
Hani Mounla <hani.mounla@outlook.com>
2019-07-14 21:33:13 +0300

    Inserted data from migrations

--- EMS/EMS.Web/Migrations/DefaultDB/DefaultDB_20190101_130000_EMS.cs
+++ EMS/EMS.Web/Migrations/DefaultDB/DefaultDB_20190101_130000_EMS.cs
@@ -8,75 +8,6 @@ namespace EMS.Migrations.DefaultDB
     {
         public override void Up()
         {
-            //// EMS Migrations
-
-            //#region Zoning
-
-            //Create.Table("City")
-            //       .WithColumn("CityID").AsInt32().Identity().PrimaryKey()
-            //       .WithColumn("Name").AsString(200).NotNullable()
-            //       .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //Create.Table("Building")
-            //        .WithColumn("BuildingID").AsInt32().Identity().PrimaryKey()
-            //        .WithColumn("ZipCode").AsString(200).Nullable()
-            //        .WithColumn("Address").AsString(200).Nullable()
-            //        .WithColumn("StreetAddress").AsString(200).Nullable()
-            //        .WithColumn("Description").AsString(200).Nullable()
-            //        .WithColumn("CityID").AsInt32().Nullable().ForeignKey("FK_Building_City", "dbo", "City", "CityID")
-            //        .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //Create.Table("Floor")
-            //        .WithColumn("FloorID").AsInt32().Identity().PrimaryKey()
-            //        .WithColumn("FloorNumber").AsInt32().Nullable()
-            //        .WithColumn("Name").AsString(200).Nullable()
-            //        .WithColumn("Describtion").AsString(200).Nullable()
-            //        .WithColumn("BuildingID").AsInt32().Nullable().ForeignKey("FK_Floor_Building", "dbo", "Building", "BuildingID")
-            //        .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //Create.Table("Section")
-            //        .WithColumn("SectionID").AsInt32().Identity().PrimaryKey()
-            //        .WithColumn("Name").AsString(200).Nullable()
-            //        .WithColumn("Describtion").AsString(200).Nullable()
-            //        .WithColumn("FloorID").AsInt32().Nullable().ForeignKey("FK_Section_Floor", "dbo", "Floor", "FloorID")
-            //        .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //Create.Table("Apartment")
-            //        .WithColumn("ApartmentID").AsInt32().Identity().PrimaryKey()
-            //        .WithColumn("Name").AsString(200).Nullable()
-            //        .WithColumn("Type").AsString(200).Nullable()
-            //        .WithColumn("Address").AsString(200).Nullable()
-            //        .WithColumn("RoomNumber").AsString(200).Nullable()
-            //        .WithColumn("Describtion").AsString(200).Nullable()
-            //        .WithColumn("SectionID").AsInt32().Nullable().ForeignKey("FK_Apartment_Section", "dbo", "Section", "SectionID")
-            //        .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //#endregion
-
-            //#region meters
-
-            //Create.Table("Meter")
-            //        .WithColumn("MeterID").AsInt32().Identity().PrimaryKey()
-            //        .WithColumn("ApartmentID").AsInt32().Nullable().ForeignKey("FK_Meter_Apartment", "dbo", "Apartment", "ApartmentID")
-            //        .WithColumn("ConsumerID").AsInt32().Nullable().ForeignKey("FK_Meter_Consumer", "dbo", "Users", "UserId")
-            //        .WithColumn("Deleted").AsBoolean().Nullable();
-
-            //Create.Table("MeterType")
-            //        .WithColumn("FloorID").AsInt32().Identity().PrimaryKey().NotNullable()
-            //        .WithColumn("BuildingID").AsString(200).NotNullable();
-
-            //Create.Table("MeterParameter")
-            //        .WithColumn("MeterParameterID").AsInt32().Identity().PrimaryKey().NotNullable()
-            //        .WithColumn("BuildingID").AsString(200).NotNullable();
-
-            //#endregion
-
-            //#region EMS_Devices
-
-
-            //#endregion
-
-
             // EMS Migrations
 
             #region Zoning
@@ -355,6 +286,34 @@ namespace EMS.Migrations.DefaultDB
             //    .WithColumn("Date").AsDateTime().NotNullable()
             //    .WithColumn("Details").AsString(int.MaxValue).Nullable();
 
+            #endregion
+
+            #region inserting data
+
+            Insert.IntoTable("City")
+                .Row(new
+                {
+                    Name = "Aleppo",
+                    Description = "The oldest city in the world"
+                })
+                .Row(new
+                {
+                    Name = "Damascus",
+                    Description = "The second oldest city in the world"
+                });
+
+
+
+            Insert.IntoTable("Building")
+                .Row(new
+                {
+                    Name = "The HQ building",
+                    CityID = "1",
+                    ZipCode = "+9846",
+                    Address = "The Address",
+                    StreetAddress = "Street address",
+                    Description = "Description"
+                });
             #endregion
         }
     }
